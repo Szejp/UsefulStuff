c2b017cd2df7d45a76780bf7e43b53ce0373ee49
Jan Kuczmowski <Janqczmowski@gmail.com>
2018-04-04 20:13:10 +0200

    *: nothing

--- Assets/ProceduralMeshes/Scripts/Helpers/Algebry.cs
+++ Assets/ProceduralMeshes/Scripts/Helpers/Algebry.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Algebry : MonoBehaviour {
 
-    private Vector3 point;
+	private Vector3 point;
 
-    [ContextMenu("GetSpherePoint")]
-    public void GetSpherePoint() {
-        point = new Vector3(110, 100, (float)GetSpherePoint(new float[] { 110, 100 }, new float[] { 100, 100, 100 }, 10));
-        Debug.Log("[MathUtis] " + point);
-    }
+	[ContextMenu("GetSpherePoint")]
+	public void GetSpherePoint() {
+		point = new Vector3(110, 100, (float)GetSpherePoint(new float[] { 110, 100 }, new float[] { 100, 100, 100 }, 10));
+		Debug.Log("[MathUtis] " + point);
+	}
 
-    public static float? GetSpherePoint(float[] knownValues, float[] origin, float range) {
-        float sqrtEq = Mathf.Pow(range, 2) - Mathf.Pow(knownValues[0] - origin[0], 2) - Mathf.Pow(knownValues[1] - origin[1], 2);
-        if (sqrtEq >= 0) {
-            return Mathf.Sqrt(sqrtEq) + origin[2];
-        }
-        else {
-            Debug.LogError("[Algebry] That point is not part of the sphere ");
-            return null;
-        }
-    }
+	public static float? GetSpherePoint(float[] knownValues, float[] origin, float range) {
+		float sqrtEq = Mathf.Pow(range, 2) - Mathf.Pow(knownValues[0] - origin[0], 2) - Mathf.Pow(knownValues[1] - origin[1], 2);
+		if (sqrtEq >= 0) {
+			return Mathf.Sqrt(sqrtEq) + origin[2];
+		}
+		else {
+			Debug.LogError("[Algebry] That point is not part of the sphere ");
+			return null;
+		}
+	}
 
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(point, 0.1f);
-    }
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(point, 0.1f);
+	}
 }
