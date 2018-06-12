e2eb2bd8cef056bb1a9f513dce4359c1e6bc7832
Jan Kuczmowski <Janqczmowski@gmail.com>
2018-06-12 22:35:16 +0200

    *: airplane to be continued

--- Assets/Physics/Scripts/AirPlane.cs
+++ Assets/Physics/Scripts/AirPlane.cs
@@ -13,10 +13,13 @@ public class AirPlane : Movement {
 	private float _wingArea = 10;
 	[SerializeField]
 	private float _speed;
+	[SerializeField]
+	private float _rotationFactor = 1;
 
 	private Vector3 _lift;
 	private Rigidbody _rb;
 	private float _thrustAcceleration;
+	private float _angleOfAttack;
 
 	protected override void Start() {
 		base.Start();
@@ -27,15 +30,25 @@ public class AirPlane : Movement {
 	private void Update() {
 		_thrustAcceleration = 0;
 #if UNITY_EDITOR
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.LeftShift))
 			_thrustAcceleration += Mathf.Clamp(_speed, 0, 300);
+		if (Input.GetKey(KeyCode.W))
+			_rb.rotation = Quaternion.AngleAxis(_rotationFactor, transform.right) * _rb.rotation;
+		if (Input.GetKey(KeyCode.S))
+			_rb.rotation = Quaternion.AngleAxis(-_rotationFactor, transform.right) * _rb.rotation;
+		if (Input.GetKey(KeyCode.S))
+			_rb.rotation = Quaternion.AngleAxis(_rotationFactor, transform.up) * _rb.rotation;
+		if (Input.GetKey(KeyCode.S))
+			_rb.rotation = Quaternion.AngleAxis(-_rotationFactor, transform.up) * _rb.rotation;
 #endif
 	}
 
 	private void FixedUpdate() {
-		Vector3 vMatters = Vector3.Dot(_rb.velocity, transform.forward) * transform.forward / _rb.velocity.sqrMagnitude;
-		_lift = _coefficent * _airDensity * _wingArea / 2 * Mathf.Pow(_rb.velocity.magnitude * Time.deltaTime + _thrustAcceleration * Time.deltaTime, 2) * transform.up;
-		Debug.Log("lift: " + _lift + " thrust acceleration: " + _thrustAcceleration);
+		_angleOfAttack = 360 - transform.rotation.eulerAngles.x;
+		_coefficent = Mathf.Clamp(-Mathf.Pow((_angleOfAttack - 15) / 13, 2) + 1.75f, 0, 2);
+		_rb.velocity += _thrustAcceleration * Time.deltaTime * _rb.velocity.normalized;
+		_lift = _coefficent * _airDensity * _wingArea / 2 * Mathf.Pow(_rb.velocity.magnitude * Time.deltaTime, 2) * transform.up;
+		Debug.Log("lift: " + _lift + " thrust acceleration: " + _thrustAcceleration + " angle of attack: " + _angleOfAttack);
 		_rb.AddForce(_lift + _thrustAcceleration * Time.deltaTime * transform.forward * _rb.mass);
 	}
 }
