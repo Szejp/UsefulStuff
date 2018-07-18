b38ebc1a70e51be20ee62d8a50d4707dda26363e
Jan Kuczmowski <Janqczmowski@gmail.com>
2018-07-18 23:01:20 +0200

    +: added touch input

---
+++ Assets/Input/TouchInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class TouchInput : MonoBehaviour, IDragHandler {
+
+	public static UnityAction<Vector2> OnDragEvent = new UnityAction<Vector2>(p => { });
+
+	public void OnDrag(PointerEventData eventData) {
+		OnDragEvent.Invoke(eventData.delta);
+	}
+}
--- Assets/Movement/SimplePlane3D/Scripts/SimplePlane3D.cs
+++ Assets/Movement/SimplePlane3D/Scripts/SimplePlane3D.cs
@@ -8,22 +8,40 @@ public class SimplePlane3D : MonoBehaviour {
 	private float torque = 10;
 	[SerializeField]
 	private float topSpeed = 200;
+	[SerializeField]
 	private Rigidbody rbody;
-	
+	[SerializeField]
+	private float turnFactor = 1;
+	private Vector2 turnVector;
+
+	public void Turn(Vector2 turnVector) {
+		this.turnVector = turnVector * turnFactor;
+		Debug.Log("[SimplePlane3D] turn vector: " + this.turnVector);
+	}
+
 	private void Awake() {
 		rbody = GetComponent<Rigidbody>();
+		TouchInput.OnDragEvent += Turn;
 	}
 
 	private void FixedUpdate() {
 		if (rbody != null) {
-			float turn = Input.GetAxis("Horizontal");
 			rbody.AddForce(rbody.transform.forward * thurst);
-			rbody.AddRelativeTorque(Vector3.up * torque * turn);
-			float turnVertical = Input.GetAxis("Vertical");
-			rbody.AddRelativeTorque(Vector3.right * torque * turnVertical);
+			rbody.AddRelativeTorque(Vector3.up * torque * turnVector.x);
+			rbody.AddRelativeTorque(Vector3.right * torque * turnVector.y);
 		}
 
 		if (rbody.velocity.magnitude > topSpeed)
 			rbody.velocity = rbody.velocity.normalized * topSpeed;
+
+		turnVector = Vector2.zero;
+	}
+
+	private void Update() {
+#if UNITY_EDITOR
+		float turnHorizontal = Input.GetAxis("Horizontal");
+		float turnVertical = Input.GetAxis("Vertical");
+	//	turnVector = new Vector2(turnHorizontal, turnVertical);
+#endif
 	}
 }
